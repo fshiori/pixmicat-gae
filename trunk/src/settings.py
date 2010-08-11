abdc58b18b782f260a20b02d5a90126442217ebb
fshiori <fshiori@33c89b58-74c4-8e44-6a02-07fee06444d1>
2010-08-11 04:32:27 +0000

    Add gaeo and base config

---
+++ trunk/src/settings.py
@@ -0,0 +1,15 @@
+# -*= coding: utf-8 -*-
+
+#/*---- Part 1：程式基本設定 ----*/
+# 伺服器常態設定
+TIME_ZONE = 'Asia/Taipei' # 時區設定
+PIXMICAT_LANGUAGE = '' # 語系語定
+
+# FileIO設定
+FILEIO_BACKEND = '' # Add 'GAE_BlobProperty', 'GAE_BlobStore' or 'ImageShack'.
+IMAGESHACK_API_KEY = '' # Only ImageShack need.
+
+#/*---- Part 2：板面各項細部功能設定 ----*/
+TITLE = 'Pixmicat!-PIO' # 網頁標題
+ADMIN_PASS = 'futaba' # 管理員密碼
+
