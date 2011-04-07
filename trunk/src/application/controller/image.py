b1b7b48d6077002a284a2701ffce6f2b219f087d
fshiori <fshiori@33c89b58-74c4-8e44-6a02-07fee06444d1>
2011-04-07 04:31:24 +0000

    Add Image get/resize

---
+++ trunk/src/application/controller/image.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from google.appengine.api import images
+
+from gaeo.controller import BaseController
+
+from model.pixmicat import Pixmicat
+
+class ImageController(BaseController):
+    
+    def index(self):
+        pass
+    
+    def get(self):
+        key = self.params.get('id')
+        entity = Pixmicat.get(key)
+        self.render(image=entity.pic)
+        
+    def resize(self):
+        key = self.params.get('id')
+        entity = Pixmicat.get(key)
+        pic = images.Image(entity.pic)
+        width = pic.width
+        height = pic.height
+        if width > 250 or height > 250:
+            if width > height:
+                rsize =  250.0 / width
+            else:
+                rsize =  250.0 / height
+            newwidth = int(width * rsize)
+            newheight = int(height * rsize)
+            pic.resize(width=newwidth, height=newheight)
+        pic = pic.execute_transforms(output_encoding=images.JPEG)
+        self.render(image=pic)
+                
+                
