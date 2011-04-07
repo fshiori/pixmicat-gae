81d95876a5f60c44c970584a0ce12f3dcb5437f0
fshiori <fshiori@33c89b58-74c4-8e44-6a02-07fee06444d1>
2011-04-07 09:12:38 +0000

    Can read reply

--- src/application/controller/post.py
+++ src/application/controller/post.py
@@ -68,6 +68,9 @@ class PostController(BaseController):
         if not title:
             title = '無標題'
         content = self.params.get('com')
+        if not content:
+            content = u'無內文'
+            #content = 'xxx'
         pic = self.params.get('upfile')
         tags = self.params.get('category')
         noimg = self.params.get('noimg')
@@ -85,6 +88,50 @@ class PostController(BaseController):
         if tags:
             data.tags = tags
         data.put()
+        self.redirect('/')
+        
+    def read(self):
+        key = self.params.get('id')
+        self.key = key
+    
+    def reply(self):
+        username = self.params.get('name')
+        if username:
+            username = _processUsername(username)
+        else:
+            username = '無名氏'
+        postip = self.request.remote_addr
+        postid = _processPostid(postip)
+        email = self.params.get('email')
+        title = self.params.get('sub')
+        if not title:
+            title = '無標題'
+        content = self.params.get('com')
+        if not content:
+            content = '無內文'
+        pic = self.params.get('upfile')
+        tags = self.params.get('category')
+        noimg = self.params.get('noimg')
+        if tags:
+            tags = _processTag(tags)
+        password = self.params.get('pwd')
+        index = _getCounter()
+        key = self.params.get('id')
+        entity = db.get(key)
+        data = Pixmicat(mainpost = entity, index=index, username=username, postid=postid, email=email, title=title, content=content, password=password, postip=postip)
+        if pic:
+            data.pic = db.Blob(pic)
+        else:
+            if noimg != 'on':
+                self.redirect('/post/noimg')
+                return
+        if tags:
+            data.tags = tags
+        data.put()
+        if email != 'sage':
+            now = datetime.datetime.now()
+            entity.replytime = now
+            entity.put()
         self.redirect('/')
         
     def noimg(self):
--- src/application/controller/welcome.py
+++ src/application/controller/welcome.py
@@ -4,9 +4,27 @@ import tz_helper
 
 from gaeo.controller import BaseController
 from google.appengine.ext import db
+from google.appengine.api import images
 
 import settings
 from model.pixmicat import Pixmicat
+
+def _resize(pic):
+    width = pic.width
+    height = pic.height
+    if width > 250 or height > 250:
+        if width > height:
+            rsize =  250.0 / width
+        else:
+            rsize =  250.0 / height
+        width = int(width * rsize)
+        height = int(height * rsize)
+    return {'width':width, 'height':height}
+
+def _getFileSize(pic):
+    size = len(pic)
+    size = size/1024
+    return size
 
 class WelcomeController(BaseController):
     """The default Controller
@@ -21,9 +39,39 @@ class WelcomeController(BaseController):
         self.title = settings.TITLE
         
         msgs = Pixmicat.all()
+        msgs.filter('mainpost =', None) 
         msgs.order('-replytime')
         msgs.fetch(10)
-        self.msgs = msgs
+        posts = []
+        for msg in msgs:
+            tmp = {}
+            tmp['key'] = msg.key()
+            tmp['content'] = msg.content
+            tmp['createtime'] = msg.createtime
+            tmp['email'] = msg.email
+            tmp['index'] = msg.index
+            tmp['pic'] = 0
+            if msg.pic:
+                tmp['pic'] = 1
+                tmp['size'] = _getFileSize(msg.pic)
+                pic = images.Image(msg.pic)
+                tmp['width'] = pic.width
+                tmp['height'] = pic.height
+                d = _resize(pic)
+                tmp['newwidth'] = d.get('width')
+                tmp['newheight'] = d.get('height')
+                tmp['resize'] = 0
+                if tmp['width'] != tmp['newwidth'] or tmp['height'] != tmp['newheight']:
+                    tmp['resize'] = 1
+            tmp['postid'] = msg.postid
+            tmp['title'] = msg.title
+            tmp['username'] = msg.username
+            replies = Pixmicat.all()
+            replies.filter('mainpost =', msg)
+            replies.order('createtime')
+            tmp['replies'] = replies
+            posts.append(tmp)
+        self.msgs = posts
         #tz = tz_helper.timezone(settings.TIME_ZONE)
         #now = datetime.datetime.now(tz)
         #self.render(text='Exception: %s' % now)
