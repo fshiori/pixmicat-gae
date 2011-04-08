aba1a8bcf415feceaec6a3c774c81c01418bf93f
fshiori <fshiori@33c89b58-74c4-8e44-6a02-07fee06444d1>
2011-04-08 05:38:27 +0000

    Let Reply display correct

--- src/application/controller/post.py
+++ src/application/controller/post.py
@@ -7,6 +7,7 @@ import hashlib
 import random
 
 from google.appengine.ext import db
+from google.appengine.api import images
 
 from gaeo.controller import BaseController
 from gaeo.session.memcache import MemcacheSession 
@@ -71,6 +72,63 @@ def _createRandom(len=10):
     tmp = ''
     for i in range(len):
         tmp += random.choice('1234567890qwertyuiopasdfghjklzxcvbnm')
+    return tmp
+
+def _resize(pic):
+    width = pic.width
+    height = pic.height
+    if width > 250 or height > 250:
+        if width > height:
+            rsize =  250.0 / width
+        else:
+            rsize =  250.0 / height
+        width = int(width * rsize)
+        height = int(height * rsize)
+    return {'width':width, 'height':height}
+
+def _resizeReply(pic):
+    width = pic.width
+    height = pic.height
+    if width > 125 or height > 125:
+        if width > height:
+            rsize =  125.0 / width
+        else:
+            rsize =  125.0 / height
+        width = int(width * rsize)
+        height = int(height * rsize)
+    return {'width':width, 'height':height}
+
+def _getFileSize(pic):
+    size = len(pic)
+    size = size/1024
+    return size
+
+def _packData(msg, type=1):
+    tmp = {}
+    tmp['key'] = msg.key()
+    tmp['content'] = msg.content
+    tmp['createtime'] = msg.createtime
+    tmp['email'] = msg.email
+    tmp['index'] = msg.index
+    tmp['pic'] = 0
+    if msg.pic:
+        tmp['pic'] = 1
+        tmp['size'] = _getFileSize(msg.pic)
+        pic = images.Image(msg.pic)
+        tmp['width'] = pic.width
+        tmp['height'] = pic.height
+        if type == 1:
+            d = _resize(pic)
+        else:
+            d = _resizeReply(pic)
+        tmp['newwidth'] = d.get('width')
+        tmp['newheight'] = d.get('height')
+        tmp['resize'] = 0
+        if tmp['width'] != tmp['newwidth'] or tmp['height'] != tmp['newheight']:
+            tmp['resize'] = 1
+    tmp['postid'] = msg.postid
+    tmp['title'] = msg.title
+    tmp['username'] = msg.username
     return tmp  
 
 class PostController(BaseController):
@@ -122,32 +180,38 @@ class PostController(BaseController):
         key = self.params.get('id')
         self.key = key
         msg = Pixmicat.get(key)
-        tmp = {}
-        tmp['key'] = msg.key()
-        tmp['content'] = msg.content
-        tmp['createtime'] = msg.createtime
-        tmp['email'] = msg.email
-        tmp['index'] = msg.index
-        tmp['pic'] = 0
-        if msg.pic:
-            tmp['pic'] = 1
-            #tmp['size'] = _getFileSize(msg.pic)
-            #pic = images.Image(msg.pic)
-            #tmp['width'] = pic.width
-            #tmp['height'] = pic.height
-            #d = _resize(pic)
-            #tmp['newwidth'] = d.get('width')
-            #tmp['newheight'] = d.get('height')
-            #tmp['resize'] = 0
-            #if tmp['width'] != tmp['newwidth'] or tmp['height'] != tmp['newheight']:
-            #    tmp['resize'] = 1
-        tmp['postid'] = msg.postid
-        tmp['title'] = msg.title
-        tmp['username'] = msg.username
+        p = self.params.get('pages')
+        if not p:
+            p = 0
+        else:
+            p = int(p)
+        #logging.info(p)
+        session = MemcacheSession(self)
+        savepassword = session.get('password')
+        #logging.info(session)
+        if savepassword:
+            #logging.info("1")
+            self.savepassword = savepassword  
+        self.title = settings.TITLE
+        totalpost = Counter.get_by_key_name('Post').count
+        res = []
+        tmp = _packData(msg)
         replies = Pixmicat.all()
         replies.filter('mainpost =', msg)
         replies.order('createtime')
-        tmp['replies'] = replies
+        for reply in replies:
+            tmp2 = _packData(reply, 2)
+            res.append(tmp2)
+        tmp['replies'] = res
+        #posts.append(tmp)
+        self.msgs = [tmp]
+        pages = totalpost / 10
+        self.pages = range(pages + 1)
+        self.nowpage = p
+        self.maxpage = pages
+        #tz = tz_helper.timezone(settings.TIME_ZONE)
+        #now = datetime.datetime.now(tz)
+        #self.render(text='Exception: %s' % now)
     
     def reply(self):
         username = self.params.get('name')
