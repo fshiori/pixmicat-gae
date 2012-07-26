6f8672c7980f84a4fe6054901f3390cea074d14c
fshiori@gmail.com <fshiori@gmail.com@33c89b58-74c4-8e44-6a02-07fee06444d1>
2012-07-26 11:19:47 +0000

    relocate data folder

--- trunk/main.py
+++ trunk/main.py
@@ -1,54 +1,7 @@
 # -*- coding: utf-8 -*-
-import string
-
 import webapp2
 
 from urls import routes
 from settings import DEBUG, config
-from handler import BaseHandler
-from models import Pixmicat
-
-class MainPage(BaseHandler):
-    def get(self, page_num=0):
-        #如果瀏覽器支援XHTML標準MIME就輸出
-        #todo
-        #有啟動Gzip
-        #todo
-        context = {}
-        context['config'] = self.app.config
-        q = Pixmicat.all()
-        q.order('-replytime')
-        PAGE_DEF = self.app.config.get('PAGE_DEF')
-        threads = q.fetch(limit=PAGE_DEF, offset=page_num * PAGE_DEF)
-        #預測過舊文章和將被刪除檔案
-        #todo
-        context['formtop'] = True
-        context['max_file_size'] = self.app.config.get('MAX_KB') * 1024
-        context['allow_upload_ext'] = string.replace(self.app.config.get('ALLOW_UPLOAD_EXT'), '|', ', ')
-        if self.app.config.get('STORAGE_LIMIT'):
-            pass #todo
-        for thread in threads:
-            res_start = thread.count - self.app.config.get('RE_DEF') + 1
-            if res_start < 1:
-                res_start = 1
-            res_amount = self.app.config.get('RE_DEF')
-            hidden_reply = res_start - 1
-            q = Pixmicat.all()
-            q.filter('mainpost =', thread)
-            q.order('createtime')
-            posts = q.fetch(limit=res_amount, offset=res_start-1)
-            for post in posts:
-                name = post.name
-        self.render_response('index.html', **context)
-        
-    def post(self):
-        #self.request.get('content')
-        name = self.request.get('name')
-        email = self.request.get('email')
-        sub = self.request.get('sub')
-        com = self.request.get('com')
-        pwd = self.request.get('pwd')
-        category = self.request.get('category')
-        
 
 app = webapp2.WSGIApplication(routes=routes, debug=DEBUG, config=config)
--- trunk/urls.py
+++ trunk/urls.py
@@ -2,9 +2,9 @@
 import webapp2
 
 routes = [
-    webapp2.Route(r'/', 'main.MainPage', name='main'),
-    webapp2.Route(r'/<page_num:\d+>/', 'main.MainPage'),
-    webapp2.Route(r'/reply/', 'reply.ReplyPage'),
-    webapp2.Route(r'/reply/<res:\d+>/', 'reply.ReplyPage'),
-    webapp2.Route(r'/reply/<res:\d+>/<page_num>/', 'reply.ReplyPage'),
+    webapp2.Route(r'/', 'pixmicat-gae.index.IndexPage', name='index'),
+    #webapp2.Route(r'/<page_num:\d+>/', 'main.MainPage'),
+    #webapp2.Route(r'/reply/', 'reply.ReplyPage'),
+    #webapp2.Route(r'/reply/<res:\d+>/', 'reply.ReplyPage'),
+    #webapp2.Route(r'/reply/<res:\d+>/<page_num>/', 'reply.ReplyPage'),
 ]
