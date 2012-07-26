5e2f1678ba942db70439b0d02decabadae4af617
fshiori@gmail.com <fshiori@gmail.com@33c89b58-74c4-8e44-6a02-07fee06444d1>
2012-07-26 09:47:32 +0000

    update some empty file for functional

--- reply.py
+++ reply.py
@@ -9,8 +9,11 @@ from handler import BaseHandler
 from models import Pixmicat
 
 class ReplyPage(BaseHandler):
-    def get(self, res=''):
+    def get(self, res='', page_num=''):
         if not res:
             return self.redirect(uri_for('main'))
-        page = 1
+        page = page_num if page_num else 'RE_PAGE_MAX'
+        if not(page == 'all' or page=='RE_PAGE_MAX'):
+            page = int(page_num)
+        #updatelog($res, $page); // 實行分頁
         self.render_response('test.html')
--- urls.py
+++ urls.py
@@ -6,5 +6,5 @@ routes = [
     webapp2.Route(r'/<page_num:\d+>/', 'main.MainPage'),
     webapp2.Route(r'/reply/', 'reply.ReplyPage'),
     webapp2.Route(r'/reply/<res:\d+>/', 'reply.ReplyPage'),
-    
+    webapp2.Route(r'/reply/<res:\d+>/<page_num>/', 'reply.ReplyPage'),
 ]
