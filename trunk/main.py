6a40f1f0985bdcc67a889ec15e0167495928b4db
fshiori@gmail.com <fshiori@gmail.com@33c89b58-74c4-8e44-6a02-07fee06444d1>
2012-07-26 07:57:27 +0000

    Add some template for piximicat

--- trunk/main.py
+++ trunk/main.py
@@ -7,7 +7,13 @@ from handler import BaseHandler
 from models import Pixmicat
 
 class MainPage(BaseHandler):
-    def get(self):
-        self.render_response('index.html')
+    def get(self, page_num=0):
+        context = {}
+        context['config'] = self.app.config
+        q = Pixmicat.all()
+        q.order('-replytime')
+        PAGE_DEF = self.app.config.get('PAGE_DEF')
+        threads = q.fetch(limit=PAGE_DEF, offset=page_num * PAGE_DEF)
+        self.render_response('index.html', **context)
 
 app = webapp2.WSGIApplication(routes=routes, debug=DEBUG, config=config)
--- trunk/urls.py
+++ trunk/urls.py
@@ -2,6 +2,9 @@
 import webapp2
 
 routes = [
-    (r'/', 'main.MainPage'),
-    #webapp2.Route(r'/<name>', 'redirect.RedirectPage'),
+    webapp2.Route(r'/', 'main.MainPage', name='main'),
+    webapp2.Route(r'/<page_num:\d+>/', 'main.MainPage'),
+    webapp2.Route(r'/reply/', 'reply.ReplyPage'),
+    webapp2.Route(r'/reply/<res:\d+>/', 'reply.ReplyPage'),
+    
 ]
